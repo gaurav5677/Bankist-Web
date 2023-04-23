6581c1b4413eb7326ee37c6f7aa7343a4f6b22fb
gaurav5677 <85819394+gaurav5677@users.noreply.github.com>
2023-04-23 19:51:45 +0530

    reveling effect on section

--- main.js
+++ main.js
@@ -242,7 +242,7 @@ nav.addEventListener('mouseout', handleHover.bind(1))
 
 const header = document.querySelector('.header');
 const navHeight = nav.getBoundingClientRect().height;
-console.log(navHeight);
+// console.log(navHeight);
 
 const stickyNav = function (entries) {
    const [entry] = entries;
@@ -255,11 +255,40 @@ const stickyNav = function (entries) {
 const headerObserver = new IntersectionObserver(stickyNav, {
    root: null,
    threshold: 0,
-   rootMargin: '-90px',
+   rootMargin: `-${navHeight}px`,
 });
 
 headerObserver.observe(header);
 
+
+
+
+
+/////////// Reveal Sections
+
+const allSections = document.querySelectorAll('.section');
+
+const reverlSection = function (entries, observer) {
+   const [entry] = entries;
+   console.log(entry);
+   if (!entry.isIntersecting) return;
+
+   entry.target.classList.remove('section--hidden');
+   observer.unobserve(entry.target);
+
+}
+
+const sectionObserver = new IntersectionObserver(reverlSection, {
+   root: null,
+   threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+
+   sectionObserver.observe(section);
+   section.classList.add('section--hidden');
+
+});
 
 
 
