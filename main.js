0ba10b6ee0cb0e60cff7ffdfc39a1c4965c20c09
gaurav5677 <85819394+gaurav5677@users.noreply.github.com>
2023-04-24 00:51:38 +0530

    Lazy Loading Img

--- main.js
+++ main.js
@@ -298,6 +298,47 @@ allSections.forEach(function (section) {
 
 
 
+// Lazy Loading Image 
+// const imgTarget = document.querySelectorAll('img[data-src');
+const imgTarget = document.querySelectorAll('img[data-src]');
+const loadImg = function (entries, observer) {
+   const [entry] = entries;
+   console.log(entry);
+
+   if (!entry.isIntersecting) return;
+   // otherwise we want to replace the src with data-src (img names Low quality to high quality )
+   entry.target.src = entry.target.dataset.src;
+
+   entry.target.addEventListener('load', function () {
+      entry.target.classList.remove('lazy-img');
+
+   });
+
+   observer.unobserve(entry.target);
+}
+
+const imgObserver = new IntersectionObserver(loadImg, {
+   root: null,
+   threshold: 0,
+   rootMargin: '-200px',
+});
+
+
+imgTarget.forEach(img => imgObserver.observe(img))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
